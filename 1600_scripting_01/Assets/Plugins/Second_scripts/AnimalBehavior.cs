74b6b53f40f80be5e8832ae2bd76745e614f587b
Unknown <Kale.allen42@gmail.com>
2018-05-21 21:00:44 -0600

    BarnYard
    Added animal Classes and used inheritance. Used unity 2018.1 So this may have issues.

---
+++ 1600_scripting_01/Assets/Plugins/Second_scripts/AnimalBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalBehavior : MonoBehaviour
+{
+
+	public Animal2 Animal2Info;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		print(Animal2Info.Health);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1600_scripting_01/Assets/Scripts/Mamal.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mamal : Animal
+{
+
+	public float Breath;
+	public int HairCount;
+	
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
