718e7de472fe75d928584c67b6b8acb346ee65fd
Kale Allen <Kale.allen42@gmail.com>
2018-05-23 18:44:21 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ 1600_scripting_01/Assets/Scripts/Ammunition.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ammunition : MonoBehaviour {
+
+    public class ammo
+    {
+        public int Energy;
+        public int Rockets;
+        public int Power;
+        public int Stamina;
+
+
+        //constructor
+        public ammo()
+        {
+            Energy = 100;
+            Rockets = 10;
+            Power = 100;
+            Stamina = 100;
+
+        }
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
