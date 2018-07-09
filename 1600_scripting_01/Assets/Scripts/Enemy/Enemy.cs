270a8dd969b6b4560c6405cd043a8336740c517a
Kale Allen <Kale.allen42@gmail.com>
2018-07-09 11:39:44 -0600

    A.I. Begining
    Fixed health issues and then added a basic Navmesh and AI work

--- 1600_scripting_01/Assets/Scripts/Enemy/Enemy.cs
+++ 1600_scripting_01/Assets/Scripts/Enemy/Enemy.cs
@@ -13,14 +13,18 @@ public class Enemy : MonoBehaviour
 	public HealthBase HealthBase;
 	public GameObject Droid;
 	public GameObject Saber;
+	public bool canDoDamage;
 	
 
 	private void Start()
 	{
-		DamageEvent = WeaponBase.WeaponDamage;
+		//DamageEvent = WeaponBase.WeaponDamage;
 		HealthEvent = HealthBase.HealthValue;
 
 	}
+	
+	
+	//Idea, make a bool for CANDodamage, then set it false for like 1 second, then set it to true, so spamming cant happen
 
 
 	private void OnTriggerEnter(Collider Saber)
--- 1600_scripting_01/Assets/Scripts/Rounds/CombatRounds.cs
+++ 1600_scripting_01/Assets/Scripts/Rounds/CombatRounds.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class CombatRounds : MonoBehaviour
 {
 
-	public int CurrentRound;
-	public WeaponBase WeaponBase;
+	//public int CurrentRound;
+	//public WeaponBase WeaponBase;
 	public float CurrentHealth;
 	public float WeaponDamage;
-	public float MaxHealth = 10.0f;
-	public string PlayerName;
+	//public float MaxHealth = 10.0f;
+	//public string PlayerName;
 	public List<GameObject> Droids;
 	public GameObject Droid;
 	public FloatData DamageEvent;
@@ -24,6 +24,26 @@ public class CombatRounds : MonoBehaviour
 		{
 			Droids[i].SetActive((true));
 		}
+
+		// IDEA Make a number for rounds then cound up rounds and spawn like 3 units, then next round up it to like 5 etc
+		//IEnumerator Ticker()   );
+		//{
+		// While (i > 0)
+		//	DisplayText.text = i.ToString();
+		//yield return WaitForSeconds(10);
+		//	i--;
+			
+		//}
+		
+		
+		//Display.text = "Round 1";
+		//yield return WaitForSeconds(10);
+		//Display.text = "";
+		
+		//Set round to 2
+		//spawn droids 
+
+		//Display.text = "Round 2";
 
 
 	}
