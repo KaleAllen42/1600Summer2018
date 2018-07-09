a59a2a3bf7fed0f76f57ad8f5c2bd5e0f7305d99
Unknown <Kale.allen42@gmail.com>
2018-07-09 13:15:45 -0600

    While Loops begining
    Made preliminary While loops

---
+++ 1600_scripting_01/Assets/Scripts/While/WhileLoop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class WhileLoop : MonoBehaviour
+{
+
+	public Text DisplayText;
+	public int i = 3;
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (    Ticker()  );
+	}
+	
+	IEnumerator Ticker()
+	{
+		while (i > 0)
+		{
+			DisplayText.text = i.ToString();
+			yield return new WaitForSeconds(1);
+			i--;
+		}
+
+		DisplayText.text = "GO!";
+		yield return new WaitForSeconds(2);
+		DisplayText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
